6ef8962bfae17922977c7329cae075a645cd69cd
Abul Khoyer <abulkhoyer69@gmail.com>
2020-05-11 00:05:29 +0600

    Solved 1004 problem from URI judge

---
+++ problem_4.js
@@ -0,0 +1,10 @@
+//Source: URI Judge (1004)
+//Read two integer values. After this, calculate the product between them and store the result in a variable named PROD. Print the result like the example below. Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.
+
+//Input
+//The input file contains 2 integer numbers.
+
+let a = prompt('type the value of a')
+let b = prompt('type the value of b')
+let PROD = parseInt(a) * parseInt(b)
+console.log(`PROD = ${PROD}\n`)
